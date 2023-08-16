5efa41fc8aed65357381884800577f6ffea4bafc
Sideeqkolade <sideeqkolade@gmail.com>
2023-08-16 02:31:47 +0100

    task 101 done

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,22 +1,28 @@
-#!/usr/bin/python3
-""" A script that returns all students sorted by average score
-"""
+#!/usr/bin/env python3
+'''Task 14's module.
+'''
 
 
 def top_students(mongo_collection):
-    """ A function that returns all students sorted by average score
-		in desc order
-    """
-
-    pipeline = mongo_collection.aggregate([
-    {
-        "$group": {
-            "_id": None,
-            "averageScore": {"$avg": "$score"}
-        }
-    },
-    {
-        '$sort': {'averageScore': -1}
-    }
-])
-    return pipeline 
+    '''Prints all students in a collection sorted by average score.
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
